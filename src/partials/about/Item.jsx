75a9cedf96e365afa55f4d74f801a2053b79c894
fiandev <wongkeras2@gmail.com>
2023-07-01 23:06:27 +0700

    feat: Update meta tag for SEO optimization

--- src/partials/about/Item.jsx
+++ src/partials/about/Item.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import ViewportArea from "../../components/interactive/ViewportArea";
 import Image from "../Image";
+import { randomID } from "../../utils/functions";
 
 export default function Item({ children, data, className }) {
   return (
@@ -11,6 +12,7 @@ export default function Item({ children, data, className }) {
           className={`p-4 rounded-md flex flex-col gap-2 ${
             className || "bg-main"
           }`}
+          key={ randomID }
         >
           <h1 className="font-bold text-slate-50 capitalize text-lg lg:text-xl xl:text-2xl">
             {data.label}
@@ -18,7 +20,7 @@ export default function Item({ children, data, className }) {
           <ul className="no-scrollbar flex flex-col items-start justify-start text-slate-100 gap-2 h-48 max-h-48 text-sm md:text-base lg:text-lg xl:text-xl overflow-y-scroll">
             {data.items.map((item) => {
               return (
-                <li className="flex gap-2 text-semibold items-center">
+                <li key={ randomID() } className="flex gap-2 text-semibold items-center">
                   {item.icon ? null : (
                     <Image
                       className="w-6 h-6 bg-slate-100 dark:bg-slate-800 p-[.1rem] rounded-full"
