d808d0aec0fbe57099c261bf9924bdb334487088
fiandev <wongkeras2@gmail.com>
2023-07-09 05:38:11 +0700

    feat: change Wrapper to client component

--- src/components/sections/Wrapper.tsx
+++ src/components/sections/Wrapper.tsx
@@ -1,7 +1,14 @@
+"use client"
 import { useTheme } from "@hooks/useTheme";
 
-export default function Wrapper({ children }: { children?: React.ReactNode }) {
+export default function Wrapper ({ children }: {
+  children?: React.ReactNode
+}) {
   const theme = useTheme();
-
-  return <div className="w-full pt-10 md:pt-14 lg:pt-10">{children}</div>;
+  
+  return (
+      <div className="w-full pt-10 md:pt-14 lg:pt-10">
+        { children }
+      </div>
+    )
 }
