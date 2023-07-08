5cf111a16c1a300a8b093b361c605dff78cf848d
fiandev <wongkeras2@gmail.com>
2023-07-08 19:19:25 +0700

    feat: add more types

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,5 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
 import "@/styles/index.css";
 import type { AppProps } from "next/app";
+
+library.add(fab);
 
 export default async function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
