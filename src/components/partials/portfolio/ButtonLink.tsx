6ad1ab69b1b6b5a51e88674b6d0971bb359e5568
fiandev <wongkeras2@gmail.com>
2023-07-08 19:40:39 +0700

    feat: add more types

--- src/components/partials/portfolio/ButtonLink.tsx
+++ src/components/partials/portfolio/ButtonLink.tsx
@@ -4,7 +4,7 @@ export default function ButtonLink({ link, icon, label, name }) {
   return (
     <a
       href={link || "#"}
-      name={name || "no name"}
+      key={name || "no name"}
       className="group/button flex gap-2 items-center transition-all duration-800 text-sm lg:text-md w-fit h-fit p-2 rounded-sm text-slate-50 bg-blue-400 outline outline-inherit hover:outline-blue-400 hover:outline-offset-2 hover:rounded-full"
     >
       <FontAwesomeIcon
