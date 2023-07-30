de127358fd0bc44abc2c7d04121579ff9b0da6b4
fiandev <wongkeras2@gmail.com>
2023-07-30 18:58:35 +0700

    feat: fix environments

--- next.config.js
+++ next.config.js
@@ -5,5 +5,11 @@ module.exports = {
     },
     typescript: {
       ignoreBuildErrors: true,
+    },
+    env: {
+      APP_ENV: process.env.APP_ENV,
+      MONGODB_CONNECTION: process.env.MONGODB_CONNECTION,
+      ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+      API_KEY: process.env.API_KEY,
     }
 };
