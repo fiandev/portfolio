d9556833fb90cf2a903db1ae73301cda2fd10460
fiandev <wongkeras2@gmail.com>
2023-07-30 16:11:43 +0700

    fix: env

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
       ignoreBuildErrors: true,
     },
     env: {
-      APP_ENV: local,
+      APP_ENV: "production",
       ENCRYPTION_KEY: "fiandev_key",
       MONGODB_CONNECTION: "mongodb+srv://fiandev:rEAjduhM3uYk9x0P@fiandev.8rpux3s.mongodb.net/?retryWrites=true&w=majority",
       API_KEY: "fiandev@secret_12345",
