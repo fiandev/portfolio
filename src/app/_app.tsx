19904b731928a21e4f95cc1ced110ed56a0a687e
Aditia Akbar Putra A <wongkeras2@gmail.com>
2023-08-09 02:40:42 +0700

    Add files via upload

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,17 +1,41 @@
+"use client";
 import { Fragment } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import "@/styles/index.css";
 
 library.add(fab);
 
 export default async function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      setLoading(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router.events]);
+
   return (
     <Fragment>
-      <Component {...pageProps} />
+           {loading ? <Preload /> :  <Component {...pageProps} />
       <Analytics />
     </Fragment>
   );
