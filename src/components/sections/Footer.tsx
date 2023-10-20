d377fd956826e389f6faa1f19fcc6c3efc006148
fiandev <wongkeras2@gmail.com>
2023-10-20 21:57:39 +0700

    feat: add preload & delete contact form

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faGithub,
