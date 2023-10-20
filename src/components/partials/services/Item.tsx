2019f9a11eebcf9b28833f917868a0b8ee591ae9
fiandev <wongkeras2@gmail.com>
2023-10-21 00:59:16 +0700

    fix: servieces

--- src/components/partials/services/Item.tsx
+++ src/components/partials/services/Item.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { faShop } from "@fortawesome/free-solid-svg-icons";
 import ButtonLink from "./ButtonLink";
-import Image from "../Image";
+// import Image from "../Image";
 
 export default function Item({
   className,
@@ -19,8 +19,8 @@ export default function Item({
     <div
       className={`${className} relative group/item hover:font-bold outline outline-main outline-sm hover:outline-md hover:outline-gray-800 dark:hover:outline-slate-50 rounded-md shadow-gray-900 dark:shadow-slate-100 w-28 w-max-28 h-24 p-4 flex flex-col gap-1 text-center justify-center border-main overflow-hidden`}
     >
-      <Image
-        srcset={thumbnail}
+      <img
+        src={thumbnail}
         className="group-hover/item:scale-125 group-hover/item:skew-x-3 transition-all duration-1000 group-hover/item:blur-sm absolute inset-0 w-max"
       />
       <div className="absolute h-36 transition-all hover:h-full inset-x-0 bottom-0 bg-slate-100 dark:bg-slate-800 flex flex-col gap-1 items-center justify-center">
