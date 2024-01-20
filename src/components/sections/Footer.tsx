eb8953c96cbba89f0559659d42078b2edd5d1768
fiandev <wongkeras2@gmail.com>
2024-01-20 23:30:02 +0700

    update: footer copyright #2

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -101,9 +101,9 @@ export default async function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © { new Date().getFullYear() }
+            © { new Date().getFullYear() }{" "}
             <a href="https://github.com/fiandev" className="hover:underline">
-              Fiandev
+              fiandev
             </a>
             . All Rights Reserved.
           </span>
