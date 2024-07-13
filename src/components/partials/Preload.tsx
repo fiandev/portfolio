2fe1ff133d55ced409b4a56a36f0fceadcba96c7
fiandev <fiandev@programmer.net>
2024-07-13 19:05:19 +0700

    feat: change bg image on light theme & add preload animation

--- src/components/partials/Preload.tsx
+++ src/components/partials/Preload.tsx
@@ -1,37 +1,7 @@
-"use client";
-import { useState, useEffect } from "react";
-
-const styles = [
-  "rounded-lg",
-  "rotate-45",
-  "rounded-2xl",
-  "rotate-180",
-  "animate-bounce",
-];
-
-export default function Preload({
-  className,
-  children,
-}: {
-  className?: string | null;
-  children?: React.ReactNode;
-}) {
-  const [Index, SetIndex] = useState<number>(0);
-
-  useEffect(() => {
-    setTimeout(() => {
-      let index = Index + 1 > styles.length - 1 ? 0 : Index + 1;
-      SetIndex(index);
-    }, 500);
-  }, [Index]);
-
+export default function Preload() {
   return (
-    <div className="w-screen h-screen absolute inset-0 z-[999999] grid place-items-center bg-slate-50 dark:bg-slate-800 hover:cursor-wait">
-      <div
-        className={`transition-all origin-center duration-300 ${styles[Index]} flex justify-center items-center bg-main w-8 h-8 ${className}`}
-      >
-        {children}
-      </div>
+    <div className="bg-slate-100 dark:bg-slate-900 flex w-screen h-screen justify-center items-center">
+      <span className="loader"></span>
     </div>
-  );
+  )
 }
--- src/components/sections/Wrapper.tsx
+++ src/components/sections/Wrapper.tsx
@@ -1,6 +1,16 @@
 "use client";
+import { useState, useEffect } from "react";
 import { GlobalThemeProvider } from "@hooks/useTheme";
+import Preload from "../partials/Preload";
 
 export default function Wrapper({ children }: { children?: React.ReactNode }) {
-  return <GlobalThemeProvider>{children}</GlobalThemeProvider>;
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(!isLoading)
+    },  1500);
+  }, []);
+
+  return isLoading ? <Preload/> : <GlobalThemeProvider>{children}</GlobalThemeProvider>;
 }
